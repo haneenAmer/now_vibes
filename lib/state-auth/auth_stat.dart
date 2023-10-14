144170283c69b85206f79d4aa66d883db83e28c9
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-10-14 15:54:10 +0300

    a

--- lib/state-auth/auth_stat.dart
+++ lib/state-auth/auth_stat.dart
@@ -1,10 +1,10 @@
-import 'dart:isolate';
-
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:now_vibes/posts/user_id.dart';
 import 'package:now_vibes/state-auth/auth_results.dart';
 
 @immutable
+
+/// this meaning 'AuthState' properties cannot be changed once they are set.
 class AuthState {
   final AuthResult? result;
   final bool isLoading;
@@ -18,7 +18,7 @@ class AuthState {
         userid = null;
 
   /// copy of the clas wih diffrient value of isLoading
-  /// we do this beaause if the resultre is null the isLoading will bw true and loading
+  /// we do this beaause if the resultre is null the isLoading will be true and loading
   AuthState copiedWithIsLoading(bool isLoading) {
     return AuthState(result: result, isLoading: isLoading, userid: userid);
   }
