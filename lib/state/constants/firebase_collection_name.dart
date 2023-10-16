5c01f6591372f535dbed8d0f87a388d2edb5e319
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-10-16 17:37:11 +0300

    i

---
+++ lib/state/constants/firebase_collection_name.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class FirebaseCollectiondName {
+  static const thumbnails = 'thumbnails';
+  static const comments = 'comments';
+  static const likes = 'likes';
+  static const users = 'users';
+  static const posts = 'posts';
+
+  const FirebaseCollectiondName._();
+}
