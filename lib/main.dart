88fa02a24008bc56a7a85b4375a8b52bc1d768cf
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-10-17 15:26:11 +0300

    e

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:now_vibes/backend/authenticator.dart';
 import 'dart:developer' as devtools show log;
+
+import 'package:now_vibes/state/providers/is_loggedin_proovider.dart';
 
 extension Log on Object {
   void log() => devtools.log(toString());
@@ -11,7 +14,7 @@ void main() async {
   WidgetsFlutterBinding
       .ensureInitialized(); // Ensure that Flutter bindings are initialized.
   await Firebase.initializeApp(); // Initialize Firebase.
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,29 +23,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          brightness: Brightness.dark,
-          indicatorColor: Colors.pink,
-          primarySwatch: Colors.grey),
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          body: Column(children: [
-            TextButton(
-                onPressed: () async {
-                  final result = await Authenticator().loginWithGoogle();
-                  result.log();
-                },
-                child: const Text('google')),
-            TextButton(
-                onPressed: () async {
-                  final result = await Authenticator().loginWithFacebook();
-                  result.log();
-                },
-                child: const Text('facebook')),
-          ]),
+        theme: ThemeData(
+            brightness: Brightness.dark,
+            indicatorColor: Colors.pink,
+            primarySwatch: Colors.grey),
+        debugShowCheckedModeBanner: false,
+        home: Consumer(builder: (context, ref, child) {
+          final isLoggedIn = ref.watch(isLoggedInProvider);
+          // اجتهاد شخصصي الاصل  : if (isLoggedIn) بس طلع ايرور
+          if (isLoggedIn) {
+            return const MainView();
+          } else {
+            return const LogginView();
+          }
+        }));
+  }
+}
+
+// when you are already logged in
+class MainView extends StatelessWidget {
+  const MainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('main view'),
         ),
+        body: const LogginView(),
       ),
+    );
+  }
+}
+
+// when you are not logged in
+class LogginView extends StatelessWidget {
+  const LogginView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('loggin view')),
+      body: Column(children: [
+        TextButton(
+            onPressed: () async {
+              final result = await const Authenticator().loginWithGoogle();
+              result.log();
+            },
+            child: const Text('google')),
+        TextButton(
+            onPressed: () async {
+              final result = await const Authenticator().loginWithFacebook();
+              result.log();
+            },
+            child: const Text('facebook')),
+      ]),
     );
   }
 }
