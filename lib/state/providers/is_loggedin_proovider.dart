2f4b0d0696fe38ad87e7d5ef21627006f68e16a6
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-10-20 22:33:05 +0300

    e

--- lib/state/providers/is_loggedin_proovider.dart
+++ lib/state/providers/is_loggedin_proovider.dart
@@ -4,7 +4,7 @@ import 'package:now_vibes/state/providers/auth_state_providers.dart';
 
 ///is not work alone but it is watch authstateprovider and
 ///dependent on auth (AuthState) result it should return bool
-/// in this case we will need ref tp listen
+/// in this case we will need ref to listen
 
 final isLoggedInProvider = Provider<bool>((ref) {
   final authState = ref.watch(authStateProvider);
